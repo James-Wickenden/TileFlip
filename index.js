d0ee7a2394bbdd521a9f82ce672f36ddfdccbd5d
James Wickenden <jameswickenden14@gmail.com>
2024-01-19 16:05:45 +0000

    drawing grid

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
-let grid= [,];
+let grid;
+paper.install(window);
 
 function setUpBoard(grid_size) 
 {
+    let tilemap = Array(grid_size).fill().map(() => Array(grid_size).fill([]));
     let canvas = document.getElementById('gridCanvas');
     let grid_width = canvas.width;
     let tile_width = grid_width / grid_size
@@ -10,11 +12,20 @@ function setUpBoard(grid_size)
 
     for (var i = 0; i < grid_size; i++) {
         for (var j = 0; j < grid_size; j++) {
-            grid[i,j] = new paper.Rectangle(i * tile_width, j * tile_width , tile_width, tile_width);
-            console.log(grid[i,j]);
+            tilemap[i][j] = new Rectangle(i * tile_width, j * tile_width , tile_width, tile_width);
+            
+            var path = new Path.Rectangle(tilemap[i][j]);
+            path.fillColor = 'red';
+            path.strokeColor = 'black';
         }
     }
+
+    console.log(tilemap);
+    view.draw();
+
+    return tilemap;
 }
 
-
-setUpBoard(5);
+window.onload = function() {
+	grid = setUpBoard(5);
+}
