15744f8afa290ba1da398f2c51821a70b6ce10f8
James Wickenden <jameswickenden14@gmail.com>
2024-01-19 17:41:55 +0000

    tiling

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ let canvas;
 let mouseX, mouseY = -1;
 let tile_width;
 let grid_size = 5;
+let inversion_size = 1;
+let key_locations = {};
 
 paper.install(window);
+
 
 function setUpBoard(grid_size) 
 {
@@ -13,7 +16,7 @@ function setUpBoard(grid_size)
     let canvas = document.getElementById('gridCanvas');
 
     let grid_width = canvas.width;
-    let tile_width = grid_width / grid_size
+    let tile_width = grid_width / grid_size;
     paper.setup(canvas);
 
     for (var i = 0; i < grid_size; i++) {
@@ -25,43 +28,70 @@ function setUpBoard(grid_size)
         }
     }
 
-    console.log(tilemap);
-
     return [tilemap, tile_width, canvas];
 };
 
-window.onload = function() {
-	let cfg = setUpBoard(grid_size);
-    grid = cfg[0];
-    tile_width = cfg[1];
-    canvas = cfg[2];
+
+function invertTile(x, y, inversion_size)
+{
+    for (var i = -inversion_size; i <= inversion_size; i++) {
+        for (var j = -inversion_size; j <= inversion_size; j++) {
+            let invertedTile_x = x + i;
+            let invertedTile_y = y + j;
+            if (invertedTile_x < 0 || invertedTile_x >= grid_size) continue;
+            if (invertedTile_y < 0 || invertedTile_y >= grid_size) continue;
+
+            let tile = grid[invertedTile_x][invertedTile_y];
+            if (tile.fillColor.green > 0) {
+                tile.fillColor = 'red';
+            }
+            else {
+                tile.fillColor = 'green';
+            }
+        }
+    }
 };
 
-function handleKeyDown(event) {
-    //console.log(event);
-    //console.log(mouseX);
-    //console.log(mouseY);
-    console.log(grid);
+
+function handleKeyDown(event)
+{
+    if ((event.key < '1' || event.key > '9') && event.key != ' ') return;
+    
     let canvas = document.getElementById('gridCanvas');
     let rect = canvas.getBoundingClientRect();
     let position_on_canvas = [mouseX - rect.left, mouseY - rect.top];
     let tile_on_canvas = [Math.floor(position_on_canvas[0]/tile_width), 
                           Math.floor(position_on_canvas[1]/tile_width)];
-
     
-    if (tile_on_canvas[0] < grid_size && tile_on_canvas[1] < grid_size) {
-        console.log(tile_on_canvas);
-        console.log(grid);
-        grid[tile_on_canvas[0]][tile_on_canvas[1]].fillColor = 'green';
-    }
-};
+    if (tile_on_canvas[0] < 0 || tile_on_canvas[0] >= grid_size) return;
+    if (tile_on_canvas[1] < 0 || tile_on_canvas[1] >= grid_size) return;
 
+    if (event.key == ' ') {
+        invertTile(tile_on_canvas[0], tile_on_canvas[1], 0);
+    }
+    else {
+        key_locations[event.key] == [tile_on_canvas[0], tile_on_canvas[1]];
+        invertTile(tile_on_canvas[0], tile_on_canvas[1], inversion_size);
+    }
+    
+
+};
 document.addEventListener('keydown', handleKeyDown);
 
-function handleMouseMove(event) {
+
+function handleMouseMove(event)
+{
     event = event || window.event;  
     mouseX = event.clientX;
     mouseY = event.clientY;
 };
-
 document.onmousemove = handleMouseMove;
+
+
+window.onload = function()
+{
+	let cfg = setUpBoard(grid_size);
+    grid = cfg[0];
+    tile_width = cfg[1];
+    canvas = cfg[2];
+};
